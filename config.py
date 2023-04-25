116f85a8fb3ae6d93f7bf020750d7fe560c58225
CyberWarriorGuru <mrogolino976@outlook.com>
2023-04-25 11:36:33 -0400

    init commit

---
+++ config.py
@@ -0,0 +1,22 @@
+OCTO_API_TOKEN = '918cdbb83c894910a9557ff656bb4a9a'
+OCTO_HTTP_SERVER = 58888
+OCTO_ID = 'Lakal-Upwork'
+OCTO_SEARCH_URL = 'https://app.octobrowser.net/api/v2/automation/profiles?search={}'.format(OCTO_ID)
+OCTO_HEADER = {
+    'X-Octo-Api-Token': OCTO_API_TOKEN
+}
+LOCAL_API = 'http://localhost:58888/api/profiles'
+UPWORK_URL = "https://www.upwork.com/nx/signup/?dest=home"
+HUNTER_APIKEY = '007444699661e2a4593a5b5a8803d53371b42857'
+FIRST_NAME = "Mark"
+LAST_NAME = "Rogolino"
+PASSWORD = "qurcjehdrod2022"
+EMAIL = "jamesmr1989+500@gmail.com"
+TITLE = "Senior Software Engineer & ML Engineer"
+SKILLS = ["python", "c++", "full-stack", "machine learning", "django", "golang", "scraping", "automation", "software development", "PyQt", "reinforcement learning", "sql"]
+PROFILE = f"I am a Senior Software Engineer with 10 years experience and I am always trying to provide high quality code and documentation of the product. \n - Amazing ability in the web and mobile based application development. Normally I can build website or mobile app structure within 3 days perfectly. \n - Great experience with System Engineering and Robotic engineering. I have excellent engineering skills, knowledge of programming languages, and the ability to design and build robotic systems. \n - Powerful Knowledge of Artificial Intelligence and AR & VR. I am very professional with Computer Vision, Natural Language Processing, Reinforcement Learning and VR and AR technologies."
+RATE = "50"
+STREET = "Guentzelstrasse 35"
+CITY = "Dalberg"
+ZIPCODE = "55595"
+PHONENUMBER = "06706 76 08 83"
